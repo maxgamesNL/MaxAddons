33ca78bf66df085656b15e36746793cdb8bc587d
maxgamesNL <72696183+maxgamesNL@users.noreply.github.com>
2022-02-01 17:46:24 +0100

    made chest, lever, redstone torch, stone button, wooden button, skull, stained hardened clay and cloal block right clickable with stonk without ghosting it. So you dont have to use another item to click on them

--- src/main/java/io/github/maxgamesNL/maxAddons/MaxAddons.java
+++ src/main/java/io/github/maxgamesNL/maxAddons/MaxAddons.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class MaxAddons
 {
     public static final String MODID = "maxaddons";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.1.1";
     public static final String NAME = "Max Addons";
     public static final String SERVER_PROXY_CLASS = "io.github.maxgamesNL.maxAddons.proxy.CommonProxy";
     public static final String CLIENT_PROXY_CLASS = "io.github.maxgamesNL.maxAddons.proxy.ClientProxy";
